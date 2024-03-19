502e66ad717fef98c5a2009fdbd329ae7294cabb
pkthunder87 <pkthunder87@gmail.com>
2024-03-19 07:54:51 -0700

    Feat: Add design font space mono

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <div className="mr-72 mt-8 bg-cream text-soft-red">Tip Calculator</div>
+    <div className="mr-72 mt-8 bg-cream text-6xl text-soft-red">
+      Tip Calculator
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      sans: "DM Sans",
+      sans: 'Space Mono',
     },
     screens: {
-      sm: "425px",
-      md: "750px",
-      lg: "1024px",
-      xl: "1280px",
-      xl2: "1440px",
+      sm: '425px',
+      md: '750px',
+      lg: '1024px',
+      xl: '1280px',
+      xl2: '1440px',
     },
     extend: {
       colors: {
-        "soft-red": "hsl(10, 79%, 65%)",
-        cyan: "hsl(186, 34%, 60%)",
-        "dark-brown": "hsl(25, 47%, 15%)",
-        "medium-brown": "hsl(28, 10%, 53%)",
-        cream: "hsl(27, 66%, 92%)",
-        "very-pale-orange": "hsl(33, 100%, 98%)",
+        'soft-red': 'hsl(10, 79%, 65%)',
+        cyan: 'hsl(186, 34%, 60%)',
+        'dark-brown': 'hsl(25, 47%, 15%)',
+        'medium-brown': 'hsl(28, 10%, 53%)',
+        cream: 'hsl(27, 66%, 92%)',
+        'very-pale-orange': 'hsl(33, 100%, 98%)',
       },
     },
   },
